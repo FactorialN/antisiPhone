5b9140d61c58bd5ed9d5a185c4dcbf0e82284489
FactorialN <factorialn@outlook.com>
2019-11-19 10:55:02 +0800

    sleep view fixed

--- antisiPhone/SleepView.swift
+++ antisiPhone/SleepView.swift
@@ -11,7 +11,53 @@ import SwiftUI
 struct SleepView: View {
     var body: some View {
         NavigationView{
-            Text("Whaaaat")
+            VStack{
+            AnimatableCards()
+                .frame(width: 400, height: 300)
+            VStack {
+                                     HStack {
+                                         VStack(alignment: .leading) {
+                                          Text("211")
+                                            .font(.largeTitle)
+                                                 .fontWeight(.black)
+                                                 .foregroundColor(.primary)
+                                                 .lineLimit(3)
+                                          Text("剩余次数")
+                                          .font(.headline)
+                                          .foregroundColor(.secondary)
+            
+                                          
+                                          }
+                                         Spacer()
+                                        Button(action: {
+                                            // What to perform
+                                            
+                                        }) {
+                                            // How the button looks like
+                                            Text("购买")
+                                                .fontWeight(.bold)
+                                                .font(.headline)
+                                                .padding()
+                                                .background(Color.blue)
+                                                .cornerRadius(20)
+                                                .foregroundColor(.white)
+                                        }
+                                        
+                                     }
+                                     .padding()
+                          }
+                .cornerRadius(CGFloat(15))
+                             .background(
+                              RoundedRectangle(cornerRadius: CGFloat(15))
+                                  .stroke(Color(.sRGB, red: 100/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 0)
+                                  .background((
+                                      Color(UIColor(named: "customBackgroundColor") ?? .white))
+                .cornerRadius(CGFloat(15))
+                                      .shadow(radius: CGFloat(15))
+                              ))
+                    .padding()
+
+            }
             .navigationBarTitle("睡觉")
         }
     }
